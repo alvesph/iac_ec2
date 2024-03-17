6707e954d0722f09930dc67210bcec285414015f
alvesph <alvesph1@gmail.com>
2024-03-16 23:53:12 -0300

    fix: change provider location

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
 module "rede_and_security" {
   source      = "./modules/rede_and_security"
   allowed_ips = var.allowed_ips
